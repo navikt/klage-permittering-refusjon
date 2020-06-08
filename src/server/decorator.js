19b3df7204a3e195345cd4472d42a71f9db283a9
Terese Haug <terese.haug@nav.no>
2020-06-08 16:45:01 +0200

    LeDekorata¸r. Legger på pmeter redirectToApp pp=true

--- src/server/decorator.js
+++ src/server/decorator.js
@@ -5,9 +5,9 @@ const { JSDOM } = jsdom;
 url = '';
 
 if (process.env.NAIS_CLUSTER_NAME === 'prod-sbs') {
-    url = 'https://www.nav.no/dekoratoren/?context=arbeidsgiver';
+    url = 'https://www.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true';
 } else {
-    url = 'https://www-q1.nav.no/dekoratoren/?context=arbeidsgiver';
+    url = 'https://www-q1.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true';
 }
 
 const requestDecorator = (callback) => request(url, callback);
