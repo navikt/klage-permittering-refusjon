367c3a183c4763b1086f80d0743f2ddeb9f1d2ff
Terese Haug <terese.haug@nav.no>
2020-08-26 12:53:20 +0200

    Legger til automatiske UU-tester

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import App from './App';
 
 it('renders without crashing', () => {
@@ -7,3 +9,12 @@ it('renders without crashing', () => {
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
+
+expect.extend(toHaveNoViolations);
+
+test('should have no a11y violations', async () => {
+    render(<App />, document.body);
+    const results = await axe(document.body);
+    expect(results).toHaveNoViolations();
+});
+
