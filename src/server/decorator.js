bb9f5a814723080a87ca5f9d3f93b9aa2e10a6b0
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2022-03-17 12:16:35 +0100

    Feil dekorator ingress i dev

--- src/server/decorator.js
+++ src/server/decorator.js
@@ -7,7 +7,7 @@ url = '';
 if (process.env.NAIS_CLUSTER_NAME === 'prod-gcp') {
     url = 'https://www.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true&level=Level4';
 } else {
-    url = 'https://www-q1.nav.no/dekoratoren/?context=arbeidsgiver&redirectToApp=true&level=Level4';
+    url = 'https://dekoratoren.dev.nav.no/?context=arbeidsgiver&redirectToApp=true&level=Level4';
 }
 
 const requestDecorator = (callback) => request(url, callback);
