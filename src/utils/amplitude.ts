11dfef950ec5cc455bb296eac17d583b1308280d
Catriona Tørklep <catriona.torklep@nav.no>
2022-05-27 15:06:33 +0200

    fortsetter å prøve å få kubernetes secret til å fungere

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -2,8 +2,8 @@ import amplitude from 'amplitude-js';
 
 const getApiKey = () => {
     return window.location.hostname === 'arbeidsgiver.nav.no'
-        ? process.env.amplitude-api-key_prod
-        : process.env.amplitude-api-key_dev;
+        ? process.env.AMPLITUDE_API_KEY_PROD
+        : process.env.AMPLITUDE_API_KEY_DEV;
 };
 
 const instance = amplitude.getInstance();
